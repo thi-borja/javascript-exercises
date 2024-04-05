f02dc9dc812090d5d4df290ec561e249e875dd85
thi-borja <thigrac@outlook.com>
2024-04-05 11:03:20 -0300

    Excercises 01 and 02 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0){
+        return "ERROR"
+    } else {
+        repeatedWord = []
+        for(i=0;i<times;i++){
+        repeatedWord.push(word);
+        }
+        return repeatedWord.join("")
+    }
 };
 
 // Do not edit below this line
